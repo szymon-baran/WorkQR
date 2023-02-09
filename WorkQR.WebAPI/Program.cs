4d0a9e3b53d50698bd20de5d00dfc9637869cb68
Szymon Baran <szymon.w.baran@gmail.com>
2023-02-09 22:12:50 +0100

    Add project files.

---
+++ WorkQR.WebAPI/Program.cs
@@ -0,0 +1,38 @@
+
+namespace WorkQR.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5002"));
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
